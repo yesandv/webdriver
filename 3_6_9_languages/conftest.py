180396825e35cf025e707f22c470b581a3d1b3ba
yesandv <grc@protonmail.com>
2021-07-31 14:12:08 +0300

    Add step 3.6.9

---
+++ 3_6_9_languages/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption("--language", action="store", default=None, help="Choose the language: ")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option("prefs", {"intl.accept_languages": user_language})
+    browser = webdriver.Chrome(options=options)
+    time.sleep(3)
+    yield browser
+    browser.quit()
---
+++ 3_6_9_languages/test_items.py
@@ -0,0 +1,8 @@
+import time
+
+
+def test_add_to_basket_button(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    browser.get(link)
+    time.sleep(3)
+    assert browser.find_element_by_class_name("btn-add-to-basket"), "Страница не содержит кнопку добавления в корзину"
