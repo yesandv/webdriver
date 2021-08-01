aad039386f3d8cc718f77ab911f5932bf8a119eb
yesandv <grc@protonmail.com>
2021-08-01 14:37:27 +0300

    Update step 3.6.9

--- 3_6_9_languages/test_items.py
+++ 3_6_9_languages/test_items.py
@@ -5,4 +5,5 @@ def test_add_to_basket_button(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
     time.sleep(3)
-    assert browser.find_element_by_class_name("btn-add-to-basket"), "Страница не содержит кнопку добавления в корзину"
+    button = browser.find_element_by_class_name("btn-add-to-basket")
+    assert button.is_displayed(), "Button is not visible"
